639947231317b9db11040832c5293f20aa174ea1
AsishRaju <vashish888@gmail.com>
2023-11-29 02:02:03 -0800

    updated department analysis

--- src/Pages/DepartmentAnalysis/DepartmentAnalysis.tsx
+++ src/Pages/DepartmentAnalysis/DepartmentAnalysis.tsx
@@ -1,112 +1,159 @@
-import { Accordion, Avatar, Table } from '@mantine/core';
+import { faker } from '@faker-js/faker';
+import { Accordion, Avatar, Button, Table,Text } from '@mantine/core';
 
 export function DepartmentAnalysis() {
 
-  return (
-    <Accordion variant="separated" radius = "sm">
-      <Accordion.Item key="x" value="y">
-      <Accordion.Control >Tech</Accordion.Control>
-      <Accordion.Panel>
-      <Table>
-                        <Table.Thead>
-                            <Table.Tr>
+    return (
+        <>
+            <Accordion variant="separated" radius="sm" defaultValue="y">
+                <Accordion.Item key="x" value="y">
+                    <Accordion.Control ><Text fw={'bold'}>Technical</Text></Accordion.Control>
+                    <Accordion.Panel>
+                        <Table>
+                            <Table.Thead>
+                                <Table.Tr>
+                                    <Table.Th>Employee</Table.Th>
+                                    <Table.Th>Employee-id</Table.Th>
+                                    <Table.Th>Name</Table.Th>
+                                    <Table.Th>Attendance</Table.Th>
+                                    <Table.Th>Action</Table.Th>
+                                </Table.Tr>
+                            </Table.Thead>
+                            <Table.Tbody>
+                                {
+                                    [1, 2, 3, 4].map((each) => {
+                                        return (
+                                            <Table.Tr key={faker.string.uuid()}>
+                                                <Table.Td>
+                                                    <Avatar size={40} radius={26} src={faker.image.avatarLegacy()} />
+                                                </Table.Td>
+                                                <Table.Td>{faker.string.numeric(5)}</Table.Td>
+                                                <Table.Td>{faker.person.fullName()}</Table.Td>
+                                                <Table.Td>
+                                                    {faker.number.int({min:0,max:5})}/5
+                                                </Table.Td>
+                                                <Table.Td><Button>Notify Manager</Button></Table.Td>
+                                            </Table.Tr>
+                                        )
+                                    })
+                                }
+
+                            </Table.Tbody>
+                        </Table>
+                    </Accordion.Panel>
+                </Accordion.Item>
+            </Accordion>
+            <Accordion variant="separated" radius="sm">
+                <Accordion.Item key="x" value="y">
+                    <Accordion.Control ><Text fw={'bold'}>Marketing</Text></Accordion.Control>
+                    <Accordion.Panel>
+                        <Table>
+                            <Table.Thead>
+                                <Table.Tr>
                                 <Table.Th>Employee</Table.Th>
-                                <Table.Th>Name</Table.Th>
-                                <Table.Th>Action</Table.Th>
-                                <Table.Th>Time</Table.Th>
-                            </Table.Tr>
-                        </Table.Thead>
-                        <Table.Tbody>
-                        <Table.Tr key="x">
-                <Table.Td>
-                   <Avatar size={40}  radius={26} /> 
-                </Table.Td>
-                <Table.Td>B</Table.Td>
-                <Table.Td>
-                    C
-                </Table.Td>
-                <Table.Td>D</Table.Td>
-            </Table.Tr>
-                        </Table.Tbody>
-                    </Table>
-      </Accordion.Panel>
-      <Accordion.Control >Marketing</Accordion.Control>
-      <Accordion.Panel>
-      <Table>
-                        <Table.Thead>
-                            <Table.Tr>
+                                    <Table.Th>Employee-id</Table.Th>
+                                    <Table.Th>Name</Table.Th>
+                                    <Table.Th>Attendance</Table.Th>
+                                    <Table.Th>Action</Table.Th>
+                                </Table.Tr>
+                            </Table.Thead>
+                            <Table.Tbody>
+                            {
+                                    [1, 2].map((each) => {
+                                        return (
+                                            <Table.Tr key={faker.string.uuid()}>
+                                                <Table.Td>
+                                                    <Avatar size={40} radius={26} src={faker.image.avatarLegacy()} />
+                                                </Table.Td>
+                                                <Table.Td>{faker.string.numeric(5)}</Table.Td>
+                                                <Table.Td>{faker.person.fullName()}</Table.Td>
+                                                <Table.Td>
+                                                    {faker.number.int({min:0,max:5})}/5
+                                                </Table.Td>
+                                                <Table.Td><Button>Notify Manager</Button></Table.Td>
+                                            </Table.Tr>
+                                        )
+                                    })
+                                }
+                            </Table.Tbody>
+                        </Table>
+                    </Accordion.Panel>
+                </Accordion.Item>
+            </Accordion>
+            <Accordion variant="separated" radius="sm">
+                <Accordion.Item key="x" value="y">
+                    <Accordion.Control ><Text fw={'bold'}>Finance</Text></Accordion.Control>
+                    <Accordion.Panel>
+                        <Table>
+                            <Table.Thead>
+                                <Table.Tr>
                                 <Table.Th>Employee</Table.Th>
-                                <Table.Th>Name</Table.Th>
-                                <Table.Th>Action</Table.Th>
-                                <Table.Th>Time</Table.Th>
-                            </Table.Tr>
-                        </Table.Thead>
-                        <Table.Tbody>
-                        <Table.Tr key="x">
-                <Table.Td>
-                   <Avatar size={40}  radius={26} /> 
-                </Table.Td>
-                <Table.Td>B</Table.Td>
-                <Table.Td>
-                    C
-                </Table.Td>
-                <Table.Td>D</Table.Td>
-            </Table.Tr>
-                        </Table.Tbody>
-                    </Table>
-      </Accordion.Panel>
-      <Accordion.Control >Finance</Accordion.Control>
-      <Accordion.Panel>
-      <Table>
-                        <Table.Thead>
-                            <Table.Tr>
+                                    <Table.Th>Employee-id</Table.Th>
+                                    <Table.Th>Name</Table.Th>
+                                    <Table.Th>Attendance</Table.Th>
+                                    <Table.Th>Action</Table.Th>
+                                </Table.Tr>
+                            </Table.Thead>
+                            <Table.Tbody>
+                            {
+                                    [1, 2, 3, 4,6,7,8].map((each) => {
+                                        return (
+                                            <Table.Tr key={faker.string.uuid()}>
+                                                <Table.Td>
+                                                    <Avatar size={40} radius={26} src={faker.image.avatarLegacy()} />
+                                                </Table.Td>
+                                                <Table.Td>{faker.string.numeric(5)}</Table.Td>
+                                                <Table.Td>{faker.person.fullName()}</Table.Td>
+                                                <Table.Td>
+                                                    {faker.number.int({min:0,max:5})}/5
+                                                </Table.Td>
+                                                <Table.Td><Button>Notify Manager</Button></Table.Td>
+                                            </Table.Tr>
+                                        )
+                                    })
+                                }
+                            </Table.Tbody>
+                        </Table>
+                    </Accordion.Panel>
+                </Accordion.Item>
+            </Accordion>
+            <Accordion variant="separated" radius="sm">
+                <Accordion.Item key="x" value="y">
+
+                    <Accordion.Control ><Text fw={'bold'}>Human Resource</Text></Accordion.Control>
+                    <Accordion.Panel>
+                        <Table>
+                            <Table.Thead>
+                                <Table.Tr>
                                 <Table.Th>Employee</Table.Th>
-                                <Table.Th>Name</Table.Th>
-                                <Table.Th>Action</Table.Th>
-                                <Table.Th>Time</Table.Th>
-                            </Table.Tr>
-                        </Table.Thead>
-                        <Table.Tbody>
-                        <Table.Tr key="x">
-                <Table.Td>
-                   <Avatar size={40}  radius={26} /> 
-                </Table.Td>
-                <Table.Td>B</Table.Td>
-                <Table.Td>
-                    C
-                </Table.Td>
-                <Table.Td>D</Table.Td>
-            </Table.Tr>
-                        </Table.Tbody>
-                    </Table>
-      </Accordion.Panel>
-      <Accordion.Control >HR</Accordion.Control>
-      <Accordion.Panel>
-      <Table>
-                        <Table.Thead>
-                            <Table.Tr>
-                                <Table.Th>Employee</Table.Th>
-                                <Table.Th>Name</Table.Th>
-                                <Table.Th>Action</Table.Th>
-                                <Table.Th>Time</Table.Th>
-                            </Table.Tr>
-                        </Table.Thead>
-                        <Table.Tbody>
-                        <Table.Tr key="x">
-                <Table.Td>
-                   <Avatar size={40}  radius={26} /> 
-                </Table.Td>
-                <Table.Td>B</Table.Td>
-                <Table.Td>
-                    C
-                </Table.Td>
-                <Table.Td>D</Table.Td>
-            </Table.Tr>
-                        </Table.Tbody>
-                    </Table>
-      </Accordion.Panel>
-    </Accordion.Item>
-    </Accordion>
-    
-  );
+                                    <Table.Th>Employee-id</Table.Th>
+                                    <Table.Th>Name</Table.Th>
+                                    <Table.Th>Attendance</Table.Th>
+                                    <Table.Th>Action</Table.Th>
+                                </Table.Tr>
+                            </Table.Thead>
+                            <Table.Tbody>
+                            {
+                                    [1, 2, 3, 4].map((each) => {
+                                        return (
+                                            <Table.Tr key={faker.string.uuid()}>
+                                                <Table.Td>
+                                                    <Avatar size={40} radius={26} src={faker.image.avatarLegacy()} />
+                                                </Table.Td>
+                                                <Table.Td>{faker.string.numeric(5)}</Table.Td>
+                                                <Table.Td>{faker.person.fullName()}</Table.Td>
+                                                <Table.Td>
+                                                    {faker.number.int({min:0,max:5})}/5
+                                                </Table.Td>
+                                                <Table.Td><Button>Notify Manager</Button></Table.Td>
+                                            </Table.Tr>
+                                        )
+                                    })
+                                }
+                            </Table.Tbody>
+                        </Table>
+                    </Accordion.Panel>
+                </Accordion.Item>
+            </Accordion>
+        </>);
 }
